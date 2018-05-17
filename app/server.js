2881be818e7c6f9e3484708e7e9453227765580e
ANDREAS VLCEK <andreas.vlcek@googlemail.com>
2018-05-17 15:33:49 +0200

    Add files via upload

---
+++ app/server.js
@@ -0,0 +1,55 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/:timestamp', function(request, response) {
+	var timestamp = request.params.timestamp;
+	response.json(getTimestampJSON(timestamp));
+});
+
+app.route('/')
+    .get(function(req, res) {
+		  res.sendFile(process.cwd() + '/views/index.html');
+    });
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+ 
+function getTimestampJSON(timestamp) {
+	var result = {
+		unix: null,
+		natural: null
+	};
+ 
+	var date;
+	if (!isNaN(parseInt(timestamp))) {
+		date = new Date(parseInt(timestamp));
+	} else {
+		date = new Date(timestamp);
+	}
+ 
+	if (!isNaN(date.getTime())) {
+		result.unix = date.getTime();
+		result.natural = getNaturalDate(date);
+	}
+ 
+	return result;
+}
+ 
+function getNaturalDate(date) {
+	var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'Obtober', 'November', 'December'];
+ 
+	return months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();
+}
